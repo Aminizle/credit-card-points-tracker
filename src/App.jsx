26cc7c477ff37cb59c3e080b110125b021fd8a36
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2025-04-01 13:11:33 -0400

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css'
+import "./App.css";
+import Message from "./components/Test";
 
 function App() {
-  
-
   return (
     <>
-       <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <div>
+        <Message></Message>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// PascalCasing
+function Message() {
+    // JSX: Javascipt XML
+    const name="Ayesha";
+    if (name)
+    return <h1>Hey Hey, {name}</h1>;
+    return <h1>Hey World</h1>
+}
+
+export default Message; 
